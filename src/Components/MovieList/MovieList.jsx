201cbc485975551821ca163b0115162921e70f37
Samuel Rebuelta-Sanchez <srebueltasanchez@srebuel-ltmbymq.internal.salesforce.com>
2024-06-17 23:36:31 -0700

    create load more button

--- src/Components/MovieList/MovieList.jsx
+++ src/Components/MovieList/MovieList.jsx
@@ -4,31 +4,42 @@ import MovieCard from "./MovieCard/MovieCard";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
 
   const apiKey = import.meta.env.VITE_API_KEY;
-  let url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`;
+
+  async function fetchMovies(page) {
+    let url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&page=${page}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setMovies((prevMovies) => [...prevMovies, ...data.results]);
+  }
 
   useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data.results[3]);
-      setMovies(data.results);
-    }
-    fetchMovies();
-  }, []);
+    fetchMovies(page);
+  }, [page]);
+
+  // Function to handle 'Load More' button click
+  const loadMoreMovies = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
   return (
-    <div className="movie-list">
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.title}
-          image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          title={movie.title}
-          rating={movie.vote_average}
-        />
-      ))}
-    </div>
+    <>
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.title}
+            image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            title={movie.title}
+            rating={movie.vote_average}
+          />
+        ))}
+      </div>
+      <div id="load-more">
+        <button onClick={loadMoreMovies}>Load More</button>
+      </div>
+    </>
   );
 };
 
