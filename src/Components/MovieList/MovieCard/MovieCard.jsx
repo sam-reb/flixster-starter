861006195ced99f25ad334956daaef600d94d705
Samuel Rebuelta-Sanchez <srebueltasanchez@srebuel-ltmbymq.internal.salesforce.com>
2024-06-18 12:02:54 -0700

    complete modal milestone

--- src/Components/MovieList/MovieCard/MovieCard.jsx
+++ src/Components/MovieList/MovieCard/MovieCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./MovieCard.css";
 import PropTypes from "prop-types";
 
-const MovieCard = ({ image, title, rating }) => {
+const MovieCard = ({ image, title, rating, onClick }) => {
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={onClick}>
       <img src={image} alt={title} />
       <h3>{title}</h3>
       <p>Rating: {rating}</p>
--- src/Components/MovieList/MovieList.jsx
+++ src/Components/MovieList/MovieList.jsx
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from "react";
 import "./MovieList.css";
 import MovieCard from "./MovieCard/MovieCard";
+import Modal from "../Modal/Modal";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
   const [page, setPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const apiKey = import.meta.env.VITE_API_KEY;
 
-  async function fetchMovies(page) {
+  async function fetchMovies(page = 1) {
     let url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&page=${page}`;
     const response = await fetch(url);
     const data = await response.json();
-    setMovies((prevMovies) => [...prevMovies, ...data.results]);
+    console.log(data.results[4]);
+
+    if (data.results && data.results.length > 0) {
+      setMovies((prevMovies) =>
+        page === 1 ? data.results : [...prevMovies, ...data.results]
+      );
+    } else {
+      if (page === 1) {
+        setMovies([]);
+      }
+    }
+
+    // setMovies((prevMovies) => [...prevMovies, ...data.results]);
   }
 
   useEffect(() => {
@@ -21,24 +36,57 @@ const MovieList = () => {
 
   // Function to handle 'Load More' button click
   const loadMoreMovies = () => {
-    setPage((prevPage) => prevPage + 1);
+    const nextPage = page + 1;
+    setPage(nextPage);
+    // fetchMovies();
   };
+
+  const filteredMovies = movies.filter(
+    (movie) => movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    // console.log("filering");
+  );
 
   return (
     <>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search Movies..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="search-input"
+        />
+      </div>
       <div className="movie-list">
-        {movies.map((movie) => (
+        {filteredMovies.map((movie) => (
           <MovieCard
-            key={movie.title}
+            key={movie.id}
             image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             title={movie.title}
             rating={movie.vote_average}
+            onClick={() => setSelectedMovie(movie)}
+            // onClick={() => console.log("clicked movie card")}
           />
         ))}
       </div>
       <div id="load-more">
         <button onClick={loadMoreMovies}>Load More</button>
       </div>
+      {selectedMovie && (
+        <Modal
+          show={selectedMovie !== null}
+          onClose={() => setSelectedMovie(null)}
+        >
+          <h2>{selectedMovie.title}</h2>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`}
+            alt={selectedMovie.title}
+            style={{ width: "100%" }}
+          />
+          <h3>Release Date: {selectedMovie.release_date}</h3>
+          <h4>Overview: {selectedMovie.overview}</h4>{" "}
+        </Modal>
+      )}
     </>
   );
 };
